80ceaec8beed8ecc678acb5fc1a83fcfae17b8cb
Mikhail Petukhov <petmik@yandex.ru>
2019-12-03 21:41:39 +0300

    Навел порядок в выводе результатов, вставил комменты

--- nn_lesson.py
+++ nn_lesson.py
@@ -12,19 +12,27 @@ data_outputs = np.array([[75, 82, 93]]).T
 X = data_inputs / np.amax(data_inputs, axis=0)
 Y = data_outputs / 100
 
+# начальное задание синапсов
 syn0 = 2*np.random.random((2, 1)) - 1
+print('Начальное задание синапсов')
 print(syn0.T)
 
-for iter in range(0, 200):
+lessons = 2000
+
+for iter in range(0, lessons):
     test_result = nonlin(np.dot(X, syn0))
     error = test_result - Y
 
     delta = error * nonlin(test_result, True)
     syn0 -= np.dot(X.T, delta)
-    print(syn0.T)
 
 print("Output After Training:")
+print('Итоговые значения синапсов')
+print(syn0.T)
+print('Достигнутый результат после ', lessons, ' уроков')
 print(test_result)
+print('Нужный результат')
+print(Y)
 
 
 
